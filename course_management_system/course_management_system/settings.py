61d478289d38e5b46d467caf565ee2987ee7a757
lukaszmp <86266335+lukaszmp@users.noreply.github.com>
2022-04-11 16:14:17 -0400

    Merge remote-tracking branch 'origin/main' into main

--- course_management_system/course_management_system/settings.py
+++ course_management_system/course_management_system/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'course_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + r"\templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
