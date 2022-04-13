aca61a1b95d72ead589a54b3701e5f09f8d378f8
StephenMiner222 <70287119+StephenMiner222@users.noreply.github.com>
2022-04-12 21:42:38 -0400

    Delete course_management_system directory

--- course_management_system/authentication/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def hi(request):
-    return HttpResponse("<h1>This is the homepage of the website</h1>")
