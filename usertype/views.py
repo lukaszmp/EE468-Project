77fa1382ee2c086d81be609a0ee90f1c5eacbbd2
lukaszmp <86266335+lukaszmp@users.noreply.github.com>
2022-04-22 10:56:05 -0400

    Features display on webpage done, visual display needs work

--- usertype/views.py
+++ usertype/views.py
@@ -5,25 +5,120 @@ from django.http import HttpResponse, HttpResponseRedirect
 from . import dbhelper
 from .forms import LoginForm
 from django.contrib.auth import authenticate, login
-
+#TODO: connect to dtp and pass helper insatce to this file, stop opening every time a button is pressed!
 
 def admin(request):
-    return render(request, 'usertype/admin.html')
-
-
-def professor(request):
-    return render(request, 'usertype/professor.html')
-
-
-def student(request):
-
     classes = {}
     context = {'list': classes}
     if (request.method == 'POST'): #https://www.etutorialspoint.com/index.php/257-django-pass-variables-from-view-to-html-template
-        print(request.POST.get("inSemester"))
+        sortBY = request.POST.get("inSort")
+        prof = request.POST.get("inProfessor")
+        semester = request.POST.get("inSemester")
+        featNum = request.POST.get("inFeature")  # should be turned to a radio button
+        if (featNum == ""):
+            print("Empty")  # be careful this wont do anything if not filled in
+            return render(request, 'usertype/admin.html', context)
+
+        featNum = int(featNum)
+        if (featNum == 1):
+            byName = False
+            byDept = False
+            bySalary = False
+
+            if (sortBY == "name"):
+                byName = True
+            elif (sortBY == "dept"):
+                byDept = True
+            elif (sortBY == "salary"):
+                bySalary = True
+
+            manager = dbhelper.dbManager()
+            manager.connect("root", "password12", "university")  # poor form to keep in code
+            feature_one_results = manager.getFeatureOne(inByName=byName, inByDept=byDept, inBySalary=bySalary)
+            i = 0
+            for (name) in feature_one_results:
+                classes[i] = name
+                i = i + 1
+
+        elif (featNum == 2):
+            manager = dbhelper.dbManager()
+            manager.connect("root", "password12", "university")  # poor form to keep in code
+            feature_two_results = manager.getFeatureTwo()
+            i = 0
+            for (name, min, max, avg) in feature_two_results:
+                classes[i] = (name + ":" + str(min) + ":" + str(max) + ":" + str(avg))
+                i = i + 1
+
+        elif (featNum == 3):
+            manager = dbhelper.dbManager()
+            manager.connect("root", "password12", "university")  # poor form to keep in code
+            feature_three_results = manager.getFeatureThree(prof, semester)
+            i = 0
+            for (name, deptName, count) in feature_three_results:
+                classes[i] = (name + ":" + deptName + ":" + str(count))
+                i = i + 1
+
+        else:
+            return render(request, 'usertype/admin.html', context)
+
+
+        return render(request, 'usertype/admin.html', context)
+
+    else:
+        return render(request, 'usertype/admin.html', context)
+
+def professor(request):
+    classes = {}
+    context = {'list': classes}
+    if (request.method == 'POST'): #https://www.etutorialspoint.com/index.php/257-django-pass-variables-from-view-to-html-template
+        prof = request.POST.get("inProfessor")
+        semester = request.POST.get("inSemester")
+        featNum = request.POST.get("inFeature") #should be turned to a radio button
+        if (featNum == ""):
+            print("Empty") #be careful this wont do anything if not filled in
+            return render(request, 'usertype/professor.html', context)
+
+        featNum = int(featNum)
+        if (featNum == 4):
+            manager = dbhelper.dbManager()
+            manager.connect("root", "password12", "university")  # poor form to keep in code
+            feature_four_results = manager.getFeatureFour(prof, semester)
+
+            i = 0
+            for (name, course, section, count) in feature_four_results:
+                classes[i] = (name + ":" + course + ":" + section + ":" + str(count)) #split string in js side for better display
+                i = i + 1
+
+            print(i)
+
+            return render(request, 'usertype/professor.html', context)
+
+        elif (featNum == 5):
+            manager = dbhelper.dbManager()
+            manager.connect("root", "password12", "university") #poor form to keep in code
+            feature_five_results = manager.getFeatureFive(prof, semester)
+
+            i = 0
+            for (studentName) in feature_five_results:
+                classes[i] = studentName
+                i = i + 1
+
+            return render(request, 'usertype/professor.html', context)
+        else:
+            return render(request, 'usertype/professor.html', context) #never return None
+
+    else:
+        return render(request, 'usertype/professor.html', context)
+
+def student(request):
+    classes = {}
+    context = {'list': classes}
+    if (request.method == 'POST'): #https://www.etutorialspoint.com/index.php/257-django-pass-variables-from-view-to-html-template
+        dept = request.POST.get("inDepartment")
+        semester = request.POST.get("inSemester")
         manager = dbhelper.dbManager()
         manager.connect("root", "password12", "university") #poor form to keep in code
-        feature_six_results = manager.getFeatureSix("CS", 1)
+        feature_six_results = manager.getFeatureSix(dept, semester)
         for (courseID, sectionID) in feature_six_results:
             classes[courseID] = sectionID
 
